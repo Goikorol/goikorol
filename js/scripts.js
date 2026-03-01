c68fe82be2949e2f66e5d4d459a98ec804683a74
Goikorol <goikorolweb@gmail.com>
2026-03-01 16:47:22 -0300

    gfwaerg

--- js/scripts.js
+++ js/scripts.js
@@ -273,4 +273,27 @@ document.querySelectorAll('.order-toggle').forEach(toggle => {
 
         sortGridByDate(category, oldestFirst);
     });
+
+
+    // ===============================
+// BOTÓN VOLVER ARRIBA
+// ===============================
+const toTopBtn = document.getElementById('toTopBtn');
+
+if (toTopBtn) {
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 300) {
+            toTopBtn.classList.add('show');
+        } else {
+            toTopBtn.classList.remove('show');
+        }
+    });
+
+    toTopBtn.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+}
 });
